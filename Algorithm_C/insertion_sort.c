5503f4b9fd7b9e7e533ea5a2a4b4a995583ad934
hamin <kyden5386@gmail.com>
2026-03-20 23:43:01 +0900

    0320

--- Algorithm_C/insertion_sort.c
+++ Algorithm_C/insertion_sort.c
@@ -1,18 +1,13 @@
 #include "sort.h"
 
 void insertion_sort(int arr[], int n) {
-    int i, key, j;
+    int i, j, key;
 
     for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-
-        // key보다 큰 원소들을 오른쪽으로 한 칸씩 이동
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        key = arr[i]; // 미정렬 부분 arr[i..n-1]의 첫번 째 데이터 선택
+        for (j = i; j > 0 && arr[j - 1] > key; j--) { // 정렬 부분에서 삽입할 위치 찾기
+            arr[j] = arr[j - 1]; // 정렬 부분의 arr[j - 1]이 크면 뒤로 한 칸 이동
         }
-        // 적절한 위치에 key 삽입
-        arr[j + 1] = key;
+        arr[j] = key; // 찾아진 위치에 선택된 데이터 삽입
     }
 }
